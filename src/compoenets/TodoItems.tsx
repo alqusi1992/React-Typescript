b210dcc3bfee85a978de8a2f34938fc90d32debf
alqusi1992 <malqusi@yahoo.com>
2022-04-08 17:16:56 +0200

    added remove to the app

--- src/compoenets/TodoItems.tsx
+++ src/compoenets/TodoItems.tsx
@@ -1,7 +1,13 @@
 import classes from './TodoItem.module.css';
 
-const todoItems: React.FC<{ text: string }> = (props) => {
-  return <li className={classes.item}>{props.text}</li>;
+const todoItems: React.FC<{ text: string; onRemoveTodo: (event: React.MouseEvent) => void }> = (
+  props,
+) => {
+  return (
+    <li className={classes.item} onClick={props.onRemoveTodo}>
+      {props.text}
+    </li>
+  );
 };
 
 export default todoItems;
--- src/compoenets/Todos.tsx
+++ src/compoenets/Todos.tsx
@@ -4,11 +4,15 @@ import TodoItems from './TodoItems';
 import Todo from '../models/todo';
 import classes from './Todos.module.css';
 
-const Todos: React.FC<{ items: Todo[] }> = (props) => {
+const Todos: React.FC<{ items: Todo[]; onRemoveTodo: (id: string) => void }> = (props) => {
   return (
     <ul className={classes.todos}>
       {props.items.map((item) => (
-        <TodoItems key={item.id} text={item.text} />
+        <TodoItems
+          onRemoveTodo={props.onRemoveTodo.bind(null, item.id)}
+          key={item.id}
+          text={item.text}
+        />
       ))}
     </ul>
   );
